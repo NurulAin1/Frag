23695486389ae0d854b87fbcd48769e824c76000
Ain <nurul_ain_26rpl@student.smktelkom-mlg.sch.id>
2019-04-01 19:35:03 +0700

    selesai

--- app/src/main/java/smktelkom_mlg/sch/id/fragment_ain/FirstFragment.java
+++ app/src/main/java/smktelkom_mlg/sch/id/fragment_ain/FirstFragment.java
@@ -21,7 +21,7 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return inflater.inflate(R.layout.activity_first_fragment, container, false);
     }
 
 }
